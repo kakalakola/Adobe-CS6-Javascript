8fc783da34371add237314a6d16e73fada14476e
Saad Azim <saad_azim@hotmail.com>
2022-01-19 00:54:27 +0600

    Added a check for the target document

--- Photoshop/Import Folder To Layer/Import Folder To Layer.js
+++ Photoshop/Import Folder To Layer/Import Folder To Layer.js
@@ -9,22 +9,45 @@
   ... er ... that's about it.
 */
 
+'use strict';
+
 importLayers=function(){
   var inputFolder=Folder.selectDialog("Select Image Folder")
       ,inputArray=inputFolder.getFiles()
       ,tmpFrame
 
+      ,targetDoc
+
       ,i=0
-
+  
       ;
-  $.writeln('Processing '+(inputArray.length)+' frames\n');
+  $.writeln('Processing '+(inputArray.length)+' files\n');
 
-  for(i=0;i<inputArray.length;i+=1){
+  if(documents.length==0){
+    tmpFrame=app.open(new File(inputArray[i++]));
+    app.activeDocument.layers[0].selected=true;
+    app.activeDocument.layers[0].copy();
+    
+    targetDoc=app.documents.add(
+        app.activeDocument.width
+        ,app.activeDocument.height
+        ,app.activeDocument.resolution
+        ,'New Document'
+        ,app.activeDocument.Mode
+        ,DocumentFill.TRANSPARENT
+      );
+    tmpFrame.close(SaveOptions.DONOTSAVECHANGES);
+    targetDoc.paste();
+  }else{
+    targetDoc=app.activeDocument;
+  }
+
+  for(i;i<inputArray.length;i+=1){
     tmpFrame=app.open(new File(inputArray[i]));
     app.activeDocument.layers[0].selected=true;
     app.activeDocument.layers[0].copy();
     tmpFrame.close(SaveOptions.DONOTSAVECHANGES);
-    app.activeDocument.paste();
+    targetDoc.paste();
   }
 
   return 'Done importing layers :D';
